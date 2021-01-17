2300d2da8dfe07e5aca7feb5b2577ed5492cadd3
yuanyoung <mryuanyoung@outlook.com>
2021-01-17 14:54:45 +0800

    FP: 完成管理员新增+修改
    Signed-off-by: yuanyoung <mryuanyoung@outlook.com>

--- src/pages/EntAdmins/index.tsx
+++ src/pages/EntAdmins/index.tsx
@@ -1,26 +1,32 @@
 import React, { useState, Dispatch, SetStateAction } from 'react';
 import AdminTable from '../../components/AdminTable/index';
 import AdminModal from '../../components/AdminModal/index';
-import {Button, Divider} from 'antd';
+import { Button, Divider } from 'antd';
 import style from './index.module.scss';
-import {AdminInfo} from '../../interface/admin';
+import { AdminInfo } from '../../interface/admin';
 
 export interface ModalProps {
     visible: boolean,
-    type: [number, number],
+    type: [number, number],   //[0]: 0=新增 1=修改   [1]: 1=管理员 2=超级管理员
     initDate?: AdminInfo,
 }
 
-const EnterpriseAdministratorsPage = () => {
+interface Props {
+    type: number
+}
 
-    const [modalProps, setModalProps] = useState<ModalProps>({visible: false, type: [0, 0]});
+const EnterpriseAdministratorsPage: React.FC<Props> = (props) => {
+
+    const { type } = props;
+
+    const [modalProps, setModalProps] = useState<ModalProps>({ visible: false, type: [0, type] });
 
     return (
         <div id={style.container}>
-            <Button id={style.addAdmin} type='primary' onClick={() => setModalProps({...modalProps, visible: true})}>新 增</Button>
+            <Button id={style.addAdmin} type='primary' onClick={() => setModalProps({ ...modalProps, visible: true })}>新 增</Button>
             <Divider />
-            <AdminTable type={2} setModalProps={setModalProps}/>
-            <AdminModal modalProps={modalProps} setModalProps={setModalProps}/>
+            <AdminTable type={type} setModalProps={setModalProps} />
+            <AdminModal modalProps={modalProps} setModalProps={setModalProps} />
         </div>
     );
 };
