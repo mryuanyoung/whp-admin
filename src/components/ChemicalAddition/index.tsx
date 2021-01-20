d5ad5fa9c989b7766d28f296f2a299b0cd55bb60
young <32934567+mryuanyoung@users.noreply.github.com>
2021-01-20 18:35:55 +0800

    FP: 完成化学品的增删改查 (#3)
    Signed-off-by: yuanyoung <mryuanyoung@outlook.com>

---
+++ src/components/ChemicalAddition/index.tsx
@@ -0,0 +1,138 @@
+import React, { Dispatch, SetStateAction, useState, useContext } from 'react';
+import { Drawer, Button, Typography, message, Form, Input, Progress } from 'antd';
+import { ChemicalInfo } from '../../interface/chemical';
+import style from './index.module.scss';
+import { addChemical } from '../../api/chemical';
+import { Titles, Items, ContentType } from '../../constant/chemical';
+const { Title } = Typography;
+
+const { TextArea } = Input;
+
+const FormCtx = React.createContext({} as any);
+
+const StepContent: React.FC<{ items: ContentType, name: string }> = (props) => {
+
+    const { items, name } = props;
+    const form = useContext(FormCtx);
+
+    return (
+        <>
+            <Title className={style.title} level={4}>{name}</Title>
+            <Form
+                scrollToFirstError
+                labelCol={{ span: 9 }}
+                form={form}
+            >
+                {
+                    items.map((item) => item.name ? (
+                        <Form.Item
+                            key={item.attr}
+                            name={item.attr}
+                            label={item.name}
+                            rules={[{ required: true, message: `请输入${item.name}!` }]}
+                        >
+                            <TextArea autoSize className={style.TextArea} />
+                        </Form.Item>
+                    ) : (
+                            <Form.Item
+                                key={item.attr}
+                                name={item.attr}
+                                label='请输入'
+                                rules={[{ required: true, message: '请输入内容!' }]}
+                            >
+                                <TextArea autoSize={{ minRows: 5 }} className={style.TextArea} />
+                            </Form.Item>
+                        ))
+                }
+            </Form>
+        </>
+    );
+};
+
+const Contents = Items.map((item, idx) => (
+    <StepContent items={item} key={idx} name={Titles[idx]} />
+));
+
+interface Props {
+    visible: boolean,
+    setVisible: Dispatch<SetStateAction<boolean>>,
+}
+
+const STEP = Math.floor(100 / 19);
+
+const ChemicalAddition: React.FC<Props> = (props) => {
+
+    const { visible, setVisible } = props;
+    const [current, setCurrent] = React.useState(0);
+    const [percent, setPercent] = useState(STEP * 2);
+    const [submitForm, setSubmitForm] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [form] = Form.useForm();
+
+    const next = async () => {
+        await form.validateFields();
+        setSubmitForm((f) => ({ ...f, ...(form.getFieldsValue()) }))
+        const p = percent + STEP;
+        setPercent(p > 100 ? 100 : p);
+        setCurrent(current + 1);
+    };
+
+    const prev = () => {
+        const p = percent - STEP;
+        setPercent(p < 0 ? 0 : p);
+        setCurrent(current - 1);
+    };
+
+    const formFinished = async () => {
+        setLoading(true);
+        addChemical(submitForm as ChemicalInfo)
+            .then(() => {
+                message.success('成功添加化学品', 2);
+            }).catch((error) => {
+                message.error('网络错误: ', 2);
+            }).finally(() => {
+                setLoading(false);
+            });
+    };
+
+    return (
+        <FormCtx.Provider value={form}>
+            <Drawer
+                width='60vw'
+                placement="right"
+                closable={false}
+                onClose={() => setVisible(false)}
+                visible={visible}
+                bodyStyle={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Progress className={style.progress} percent={percent} />
+                <div id={style.content}>
+                    {Contents[current]}
+                </div>
+                <div id={style.btns}>
+                    {current < Titles.length - 1 && (
+                        <Button type="primary" onClick={next}>
+                            下一页
+                        </Button>
+                    )}
+                    {current === Titles.length - 1 && (
+                        <Button type="primary" onClick={formFinished} loading={loading}>
+                            提交
+                        </Button>
+                    )}
+                    {current > 0 && (
+                        <Button style={{ margin: '0 8px' }} onClick={prev}>
+                            上一页
+                        </Button>
+                    )}
+                </div>
+            </Drawer>
+        </FormCtx.Provider>
+    )
+}
+
+export default ChemicalAddition;
--- src/pages/Chemicals/index.tsx
+++ src/pages/Chemicals/index.tsx
@@ -1,8 +1,155 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
+import { UserInfoCtx } from '../../App';
+import { Table, Space, message, Modal, Button, Input } from 'antd';
+import { getChemicalList, addChemical, deleteChemical, updateChemical } from '../../api/chemical';
+import { PAGELIMIT } from '../../constant/index';
+import { ChemicalForm } from '../../interface/chemical';
+import ChemicalDetail from '../../components/ChemicalDetail/index';
+import ChemicalAddition from '../../components/ChemicalAddition';
+import style from './index.module.scss';
+import { FieldTimeOutlined, ExperimentOutlined, EditOutlined, RedoOutlined, SettingOutlined } from '@ant-design/icons';
 
-const ChemicalsPage = () => {
+const { Column } = Table;
+const {Search} = Input;
+
+const ChemicalsPage: React.FC = () => {
+    const { userInfo } = useContext(UserInfoCtx);
+    const [loading, setLoading] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const [addVisible, setAddVisible] = useState(false);
+    const [total, setTotal] = useState(0);
+    const [data, setData] = useState<Array<ChemicalForm>>([]);
+    const [page, setPage] = useState(1);
+    const [drawerData, setDrawerData] = useState<ChemicalForm>({} as ChemicalForm);
+    const [modal, contextHolder] = Modal.useModal();
+
+    const getList = useCallback(({ page, key = null, limit = PAGELIMIT}) => {
+        setLoading(true);
+        getChemicalList({ page, limit, key })
+            .then(({ total, data: list }) => {
+                setTotal(total);
+                if (data.length === 0) {
+                    list.length = total;
+                    setData(list);
+                }
+                else {
+                    setData((data) => {
+                        data.length = total;
+                        return data;
+                    })
+                    setData(data.splice((page - 1) * limit, limit, ...list))
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                message.error('网络错误请稍后重试', 2);
+            })
+            .finally(() => setLoading(false))
+    }, []);
+
+    useEffect(() => {
+        getList({ page: 1 });
+    }, []);
+
+    const handleDetail = (value: ChemicalForm) => {
+        setDrawerData(value);
+        setVisible(true);
+    };
+
+    const handleDelete = async(id: number) => {
+        modal.confirm({
+            title: '确认删除',
+            content: `确认删除此化学品?`,
+            okText: '确定',
+            cancelText: '取消',
+            onOk: async () => {
+                setLoading(true);
+                await deleteChemical(id);
+                await getList({ page });
+            }
+        });
+    }
+
+    const handleAdd = () => {
+        setAddVisible(true);
+    };
+
+    const handleSearch = async(value: string) => {
+        getList({page: 1, key: value})
+    }
+
     return (
-        <div>化学品</div>
+        <div>
+            <div id={style.header}>
+                <Search
+                    style={{ width: '25vw' }}
+                    enterButton
+                    onSearch={handleSearch}
+                    placeholder='请输入化学品的名称或CAS号'
+                />
+                <Button onClick={handleAdd} type='primary'>新增</Button>
+            </div>
+            <Table
+                rowKey='cas'
+                loading={loading}
+                sticky
+                dataSource={data}
+                pagination={{
+                    position: ['bottomCenter'],
+                    showQuickJumper: true,
+                    total
+                }}
+                onChange={(pagination, filters, sorter, extra) => {
+                    setPage(pagination.current || 1);
+                    getList({ page: pagination.current });
+                }}
+            >
+                <Column
+                    align='center'
+                    title={<><EditOutlined /><br /><span>中文名称</span></>}
+                    dataIndex="cn_name"
+                    key="cn_name"
+                />
+                <Column
+                    align='center'
+                    title={<><ExperimentOutlined /><br /><span>cas号</span></>}
+                    dataIndex="cas"
+                    key="cas"
+                />
+                <Column
+                    align='center'
+                    title={<><RedoOutlined /><br /><span>分子式</span></>}
+                    dataIndex="formula"
+                    key="formula"
+                />
+                <Column
+                    align='center'
+                    title={<><FieldTimeOutlined /><br /><span>外观与性状</span></>}
+                    dataIndex="look"
+                    key="look"
+                />
+                <Column
+                    align='center'
+                    title={<><FieldTimeOutlined /><br /><span>气味</span></>}
+                    dataIndex="smell"
+                    key="smell"
+                />
+                <Column
+                    align='center'
+                    title={<><SettingOutlined /><br /><span>操作</span></>}
+                    key="action"
+                    render={(text: ChemicalForm) => (
+                        <Space size='small'>
+                            <a onClick={() => handleDetail(text)}>详情</a>
+                            <a onClick={() => handleDelete(text.id)}>删除</a>
+                        </Space>
+                    )}
+                />
+            </Table>
+            <ChemicalDetail visible={visible} setVisible={setVisible} data={drawerData} />
+            <ChemicalAddition visible={addVisible} setVisible={setAddVisible}/>
+            {contextHolder}
+        </div>
     );
 };
 
